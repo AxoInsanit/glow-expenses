fdf57db4ac9befea1d9fba5997f3950efbee0e0e
mdaskalovTitleX <mdaskalov@titlextechnology.com>
2014-07-25 09:47:32 +0300

    Show image in edit view.

--- GlowExpense/app/scripts/expenses/controllers/addEditExpenseCtrl.js
+++ GlowExpense/app/scripts/expenses/controllers/addEditExpenseCtrl.js
@@ -8,7 +8,6 @@ angular.module('Expenses')
 
             $scope.errorMessage = addExpenseErrorMsg;
             $scope.showErrorMessage = false;
-            $scope.imageSelectedPath = '';
 
             $scope.currencies = currenciesSvc.get();
 
--- GlowExpense/app/scripts/expenses/controllers/editExpenseCtrl.js
+++ GlowExpense/app/scripts/expenses/controllers/editExpenseCtrl.js
@@ -9,6 +9,13 @@ angular.module('Expenses')
             $scope.expense = editExpenseSvc.getExpenseForEdit();
             $scope.date = $scope.expense.date;
 
+            $scope.imageSelectedPath = null;
+
+            if ($scope.expense.imageType !== 'void'){
+                $scope.imageSelectedPath = './scripts/expenses/views/img.jpg';
+            }
+
+
             $scope.takePhoto = function() {
                 cameraSvc.takePhoto().then(function(result){
                     $scope.imageSelectedPath = result;
