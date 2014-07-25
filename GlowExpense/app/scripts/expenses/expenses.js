aba0b8937bad2caae312ea967576c3fa2feaa3c3
Dimitar Jilanov <DJilanov@gmail.com>
2014-07-25 13:26:51 +0300

    Update new mockey

--- GlowExpense/app/scripts/expenses/expenses.js
+++ GlowExpense/app/scripts/expenses/expenses.js
@@ -6,7 +6,10 @@ angular.module('Expenses', [])
     .constant('selectMode','_SELECT_')
     .constant('selectModeActivated','_SELECT_MODE_ACTIVATED')
     .constant('detailsModeActivated','_DETAILS_MODE_ACTIVATED')
-    .constant('addExpenseErrorMsg','Please complete all fields!');
-    
+    .constant('addExpenseErrorMsg','Please complete all fields!')
+    .constant('addExpensesTitle','Add Expenses')
+    .constant('addExpensesButtonLabel','Add')
+    .constant('editExpensesTitle','Edit Expenses')
+    .constant('editExpensesButtonLabel','Edit');
 
 
