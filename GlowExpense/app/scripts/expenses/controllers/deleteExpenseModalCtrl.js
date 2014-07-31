27b02a77f777f5af9795e42c2740fba394859390
Dimitar Jilanov <DJilanov@gmail.com>
2014-07-31 16:51:11 +0300

    Mege html css reports base page 2

--- GlowExpense/app/scripts/expenses/controllers/deleteExpenseModalCtrl.js
+++ GlowExpense/app/scripts/expenses/controllers/deleteExpenseModalCtrl.js
@@ -3,27 +3,33 @@
 angular.module('Expenses').controller('deleteExpModalCtrl', ['$scope', '$modalInstance', 'item', '$location',
         function ($scope, $modalInstance, item, $location) {
         	function onFetchSuccess() {
+        		//when we fetch everything correctly after the delete and fetch call
                 $modalInstance.dismiss('cancel');
             }
 
             function onFetchFail() {
+            	//when we cant catch correctly after the delete and fetch call
                 $modalInstance.dismiss('cancel');
             }
 
             function onSuccess() {
+            	//when we delete correctly and fetch call
                 item.expensesRepositorySvc.getExpenses(null, onFetchSuccess, onFetchFail);
             }
 
             function onFail() {
+            	//when we fail delete 
                 alert("Could not connect");
                 $modalInstance.dismiss('cancel');
             }
 
             $scope.ok = function () {
+            	//when we click ok
                 item.expensesRepositorySvc.deleteExpense({"token":localStorage.getItem("session-token"),"expenseId":item.expenseId.expenseId}, onSuccess, onFail);
             };
 
             $scope.cancel = function () {
+            	//when we click cancel
                 $modalInstance.dismiss('cancel');
             };
               
--- GlowExpense/app/scripts/header/controllers/signOutModalCtrl.js
+++ GlowExpense/app/scripts/header/controllers/signOutModalCtrl.js
@@ -1,6 +1,7 @@
 var SignOutModalCtrl = function ($scope, $modalInstance, items, expensesRepositorySvc, $location) {
 	$scope.profileName = localStorage.getItem("userName");
   	$scope.ok = function () {
+  		//null the token , go to login and dismiss modal
 	  	localStorage.setItem("session-token",null)
 	  	$location.path('/login');
 	  	$modalInstance.dismiss('cancel');
