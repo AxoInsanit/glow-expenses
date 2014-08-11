b6f54817746a3061668cbc4edc36febf12dacb9d
mdaskalovTitleX <mdaskalov@titlextechnology.com>
2014-08-11 12:47:02 +0300

    Refactored reports logic.

--- GlowExpense/app/scripts/expenses/services/expenseReportsDialog/expenseReportsDialogSvc.js
+++ GlowExpense/app/scripts/expenses/services/expenseReportsDialog/expenseReportsDialogSvc.js
@@ -11,22 +11,8 @@ angular.module('Expenses').factory('expenseReportsDialogSvc', ['$modal', 'report
                     $scope.reports = [];
                     $scope.searchedReport = null;
 
-//                    var statesWhiteList = [
-//                        'Draft Expense',
-//                        'Rejected by Finance',
-//                        'Rejected by Manager',
-//                        'Rejected to Submitter'
-//                    ];
-//
-//                    function checkIfInState(report){
-//                        var result = statesWhiteList.indexOf(report.state) > -1;
-//                        return result;
-//                    }
-
                     reportsSharingSvc.getReports().then(function(response){
                         $scope.reports = response.filter(filterReportByStateSvc.checkIfInState);
-
-                        debugger;
                     });
 
                     $scope.selectReport = function(report) {
