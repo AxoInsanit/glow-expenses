5e80b8c228955b97929ef045d9aaeede42ef8cfc
Dimitar Jilanov <DJilanov@gmail.com>
2014-08-17 19:24:40 +0300

    Backward animation

--- GlowExpense/app/scripts/directives/back-button.js
+++ GlowExpense/app/scripts/directives/back-button.js
@@ -5,7 +5,16 @@ angular.module('Directives').directive('backButton', function() {
         restrict: 'A',
         link: function(scope, element) {
             element.on('click', function() {
+                //we go back in the history
                 history.back();
+                //we add the controlling class
+                var element = document.getElementsByTagName('body')[0];
+                element.className = element.className + ' inApp';
+                //animation is 0.75 seconds so when animation is over we remove the controll class
+                setTimeout(function(){
+                    //we remove the controlling class
+                    document.getElementsByTagName('body')[0].className = 'ng-scope';
+                }, 1000);
             });
         }
     };
