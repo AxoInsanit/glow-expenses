6989c35ced928faecab39786bf7d3a9aee57d1e6
Dimitar Jilanov <DJilanov@gmail.com>
2014-08-29 17:08:26 +0300

    New loader

--- GlowExpense/app/scripts/api/api.js
+++ GlowExpense/app/scripts/api/api.js
@@ -13,7 +13,7 @@ angular.module('Api', [])
     // .constant('baseUrlMockeyIOS', 'localhost:8080')
     // .constant('baseUrlMockeyWeb', 'http://127.0.0.1:7000')
     //globant end point
-    .constant('baseUrlMockeyWeb', 'http://esb.dev.corp.globant.com:8080') 
+    // .constant('baseUrlMockeyWeb', 'http://esb.dev.corp.globant.com:8080') 
 
     .constant('loginUrl', '/system/login')
     .constant('expensesUrl', '/expense')
