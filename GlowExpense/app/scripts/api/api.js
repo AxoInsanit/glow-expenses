56aa4394548bc55e86ad3b1754dc02c78945ea48
Dimitar Jilanov <DJilanov@gmail.com>
2014-08-29 11:03:53 +0300

    Fix of the problem with the more numbers currency

--- GlowExpense/app/scripts/api/api.js
+++ GlowExpense/app/scripts/api/api.js
@@ -2,18 +2,18 @@
 
 angular.module('Api', [])
     .config(function () {})
-    .constant('baseUrl', 'https://esb.dev.corp.globant.com')
+    //.constant('baseUrl', 'https://esb.dev.corp.globant.com')
     //our network
-    // .constant('baseUrlMockeyWeb', 'http://10.10.10.123:7000')
+     .constant('baseUrlMockeyWeb', 'http://10.10.10.123:7000')
     //public network
-   // .constant('baseUrlMockeyWeb', 'http://92.247.83.122:7000')
+    // .constant('baseUrlMockeyWeb', 'http://92.247.83.122:7000')
     //localhost
-   .constant('baseUrlMockeyWeb', 'http://127.0.0.1:7000')
+    //.constant('baseUrlMockeyWeb', 'http://127.0.0.1:7000')
     // .constant('baseUrlMockeyEmulator', 'http://10.0.3.2:8080')
     // .constant('baseUrlMockeyIOS', 'localhost:8080')
     // .constant('baseUrlMockeyWeb', 'http://127.0.0.1:7000')
     //globant end point
-    //.constant('baseUrlMockeyWeb', 'http://esb.dev.corp.globant.com:8080') 
+    .constant('baseUrlMockeyWeb', 'http://esb.dev.corp.globant.com:8080') 
 
     .constant('loginUrl', '/system/login')
     .constant('expensesUrl', '/expense')
