a47007215d3d131b707e10e33dbdac7bfbfe6383
Miguel Frias <isc.miguelfrias@gmail.com>
2015-01-02 16:23:33 -0600

    Fix 44 - Implement ngfilter for date when creating expenses

--- GlowExpense/app/scripts/expenses/controllers/addExpenseCtrl.js
+++ GlowExpense/app/scripts/expenses/controllers/addExpenseCtrl.js
@@ -5,12 +5,12 @@ angular.module('Expenses')
         'expensesRepositorySvc', 'editSaveExpenseDialogSvc', 'getIdFromLocationSvc', 'reportExpensesRepositorySvc',
         'errorDialogSvc', 'errorMessageSvc', 'errorHandlerDefaultSvc', 'localStorageSvc', 'sessionToken', 'expenseSvc', 'cameraSvc',
         'invoiceImageRepositorySvc', 'cameraSelectDialog', 'validateNumbersSvc', 'baseUrlMockeyWeb', 'expensesUrl', 'imagesUrl',
-        'expensePostImageSvc',
+        'expensePostImageSvc', '$filter',
         function ($scope, $location, addExpensesTitle, addExpensesButtonLabel, reportsSharingSvc,
           expensesRepositorySvc, editSaveExpenseDialogSvc, getIdFromLocationSvc, reportExpensesRepositorySvc,
           errorDialogSvc, errorMessageSvc, errorHandlerDefaultSvc, localStorageSvc, sessionToken, expenseSvc,
           cameraSvc, invoiceImageRepositorySvc, cameraSelectDialog, validateNumbersSvc, baseUrlMockeyWeb, expensesUrl, imagesUrl,
-          expensePostImageSvc) {
+          expensePostImageSvc, $filter) {
 
             $scope.title = addExpensesTitle;
             $scope.buttonLabel = addExpensesButtonLabel;
@@ -34,11 +34,6 @@ angular.module('Expenses')
 
             $scope.save = function(form, expense) {
                 var newExpense = expenseSvc.create(expense);
-
-                function getformatDate(date) {
-                    // 2013-05-10
-                    return '' + date.getFullYear() + '-' + (1 + date.getMonth()+ 1) + '-' + date.getDate();
-                }
 
                 function createExpenseSuccess(response, responseHeaders){
                     var headers = responseHeaders();
@@ -84,7 +79,7 @@ angular.module('Expenses')
                 // TODO uncomment when tested with real services with working upload image
                 if(form.$valid && validateNumbersSvc.validate(expense))// && $scope.imageSelectedPath)
                 {
-                    newExpense.date = getformatDate(new Date());
+                    newExpense.date = $filter('date')(new Date(), 'yyyy-MM-dd');
                     newExpense.originalCurrency =  expense.currency.id;
                     newExpense.owner = localStorageSvc.getItem('userName');
 
