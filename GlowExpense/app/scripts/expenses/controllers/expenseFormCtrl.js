41fe2e02c960bf96c3dfcf27dabea3f30b801cb2
unknown <f.villarroel@glb-d3672.globant.com>
2015-03-11 15:09:04 -0300

    #124 - I cannot see the uploaded attachment to a report expense

--- GlowExpense/app/scripts/expenses/controllers/expenseFormCtrl.js
+++ GlowExpense/app/scripts/expenses/controllers/expenseFormCtrl.js
@@ -130,7 +130,8 @@ angular.module('Expenses')
                 transitionService.go({
                     name: 'viewExpenseImage',
                     params: {
-                        expenseId: expense.expenseId
+                        expenseId: expense.expenseId,
+                        reportId: $scope.reportId
                     },
                     direction: 'forward'
                 });
