e4de044e66b31107e00838a1836653aad8dd00e5
jm-ojeda <ojeda.juanm@gmail.com>
2015-06-08 17:53:48 -0300

    Replace hammer by idangerous

--- GlowExpense/app/scripts/directives/swipeAndSnap.js
+++ GlowExpense/app/scripts/directives/swipeAndSnap.js
@@ -1,3 +1,4 @@
+/* jshint-W117 */
 'use strict';
 
 // Define the angular module.
@@ -7,165 +8,28 @@ angular.module('Directives', [])
             scope: {},
             link: function (scope, element, attr) {
 
-                var snapCount = parseInt(attr.swipeAndSnap, 10),
-                    restPosition = 0, // Define the location to end.
-                    positionX = 0,
-                    Hammer = window.Hammer,
-                    activeView = parseInt(attr.activeView, 10),
-                    snapLocations = [], // The current position.
-                    tolerance = window.innerWidth * 0.5, //pixels measure
-                    viewChanged = false,
-                    orientation, //user is scrolling vertically or horizontally
-                    swipeLocked, //lock swipe left-rigth if user is scrolling horizontally
-                    swipeEvent;
-                    //timeToWait;
-
-                for (var i = 0; i < snapCount; i += 1) {
-                    snapLocations.push((-1) * i * window.innerWidth);
-                }
-                element.css('width', (snapCount * 100) + '%');
-
-                var calculate_snap_location = function (position) {
-                    //extend to n views
-                    var moveToView = (Math.abs(position) < tolerance) ? 0 : Math.floor( (Math.abs(position) + tolerance) / (tolerance * 2));
-                    if (moveToView !== activeView){
-                        activeView = moveToView;
-                        viewChanged = true;
-                    }
-                    return snapLocations[activeView];
-                };
-
-                function translateView(x) {
-                    element.css('-webkit-transform', 'translate3d(' + x + 'px,0px,0px)');
-                    element.css('transform', 'translate3d(' + x + 'px,0px,0px)');
-                }
-
-                function swipeLocation(ev) {
-                    positionX = restPosition + parseInt(ev.deltaX || 0, 10);
-                    if ((positionX <= snapLocations[0]) && (positionX >= snapLocations[snapLocations.length - 1])) {
-                        translateView(positionX);
-                    }
-                }
-
-                /*function notifyViewChange() {
-                    if (attr.onPanEnd) {
-                        scope.$parent.$eval(attr.onPanEnd, {$activeView: activeView});
-                    }
-                }*/
-
-                Hammer(element[0]).on('swiperight', function () {
-                    element.addClass('swipe-animate');
-                    element.removeClass('overflow-disable');
-                    swipeEvent = true;
-
-                    var previous = activeView - 1;
-                    if (previous >= 0) {
-                        activeView = previous;
-                        scope.$parent.setActiveview(activeView);
-                        restPosition = snapLocations[activeView];
-                        translateView(snapLocations[activeView]);
-                    }
-
-                });
-
-                Hammer(element[0]).on('swipeleft', function () {
-                    element.addClass('swipe-animate');
-                    element.removeClass('overflow-disable');
-                    swipeEvent = true;
-
-                    var next = activeView + 1;
-                    if (next < snapLocations.length) {
-                        activeView = next;
-                        scope.$parent.setActiveview(activeView);
-                        restPosition = snapLocations[activeView];
-                        translateView(snapLocations[activeView]);
-                    }
-
-                });
-
-                /**
-                 * Perform any setup for the drag actions.
-                 */
-                Hammer(element[0]).on('panstart', function() {
-                    // We dont want an animation delay when dragging.
-                    element.removeClass('swipe-animate');
-                    viewChanged = false;
-                    orientation = false;
-                    swipeEvent = false;
-                    //$timeout.cancel(timeToWait);
-                });
-
-                /**
-                 * Follow the drag position when the user is interacting.
-                 */
-                Hammer(element[0]).on('panmove', function(ev) {
-                    if (!orientation){
-                        var angle = Math.abs(ev.angle) % 180,
-                            result = (angle < 90) ? angle : 180 - angle;
-                        swipeLocked = (result > 20);
-                        orientation = true;
-                        if (!swipeLocked){
-                            element.addClass('overflow-disable');
-                        }
-                    }
-                    if (!swipeLocked) {
-                        var requestAnimFrame = window.requestAnimationFrame ||
-                                window.webkitRequestAnimationFrame ||
-                                window.mozRequestAnimationFrame ||
-                                window.oRequestAnimationFrame ||
-                                window.msRequestAnimationFrame ||
-                                function(callback){
-                                    window.setTimeout(callback, 1000 / 60);
-                                };
-                        var cancelAnimFrame = window.cancelAnimationFrame ||
-                                window.webkitCancelRequestAnimationFrame ||
-                                window.webkitCancelAnimationFrame ||
-                                window.mozCancelRequestAnimationFrame || window.mozCancelAnimationFrame ||
-                                window.oCancelRequestAnimationFrame || window.oCancelAnimationFrame ||
-                                window.msCancelRequestAnimationFrame || window.msCancelAnimationFrame ||
-                                clearTimeout;
-                        var requestAnimId;
-
-                        (function animate() {
-                            requestAnimId = requestAnimFrame(animate);
-                            swipeLocation(ev);
-                            cancelAnimFrame(requestAnimId);
-                        }());
-                    }
-                });
-
-                /**
-                 * The drag is finishing so we'll animate to a snap point.
-                 */
-                Hammer(element[0]).on('panend pancancel', function() {
-                    if(!swipeLocked && !swipeEvent){
-                        element.addClass('swipe-animate');
-                        element.removeClass('overflow-disable');
-                        // Work out where we should "snap" to.
-                        positionX = (positionX > snapLocations[0]) ? snapLocations[0] : positionX;
-                        positionX = (positionX < snapLocations[snapLocations.length - 1]) ? snapLocations[snapLocations.length - 1] : positionX;
-                        restPosition = calculate_snap_location(positionX);
-                        translateView(restPosition);
-                        if (viewChanged) {
-                            //Change title border instantly but wait for animation to finish before changing route.
-                            scope.$parent.setActiveview(activeView);
-                            /* timeToWait = $timeout(function () {
-                                notifyViewChange();
-                            }, 600); //wait animation to finish*/
-                        }
-                    }
-                });
-
-                translateView(snapLocations[activeView]);
-                restPosition = snapLocations[activeView];
+                var activeView = parseInt(attr.activeView, 10);
 
                 scope.$on('swipeAndSnap::changeView', function (e, viewIndex) {
                     activeView = viewIndex;
-                    element.addClass('swipe-animate');
-                    element.removeClass('overflow-disable');
-                    translateView(snapLocations[activeView]);
-                    restPosition = snapLocations[activeView];
+                    mySwiper.slideTo( activeView,500 /*transition duration*/, false /*callback*/);
                 });
+
+                function notifyViewChange() {
+                    if (attr.onPanEnd) {
+                        scope.$parent.$eval(attr.onPanEnd, {$activeView: activeView});
+                    }
+                }
+
+                var mySwiper  = new Swiper('.swiper-container',{
+                    onSlideChangeEnd : function() {
+                        activeView = mySwiper.activeIndex;
+                        notifyViewChange();
+                    },
+                    resistanceRatio: 0,
+                    slidesPerView: 1
+                });
+                mySwiper.slideTo(activeView, 0 /*transition duration*/, false /*callback*/);
             }
         };
     });
